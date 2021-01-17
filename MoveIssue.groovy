e8ddb7b7f544729eb87f347c27b83a6ec164c504
Tomas Stepanavicius <t.stepanavicius@gmail.com>
2021-01-17 14:02:41 +0200

    Initial commit
    TODO and Requirements set

---
+++ MoveIssue.groovy
@@ -0,0 +1,13 @@
+/*
+    Script for Issue move function from SD to Jira Software Project
+    for all users
+*/
+
+// TODO: 
+//  move an active issue from SD to specified projects
+
+// Requirements:
+//  user has only jira software license (not SD)
+//  Move should be available if person is an approver in Issue
+//  It should have a single choice custom field with project names (destination project)
+
